65adf0102874abf835d11f0f4464620edc0179c6
Alice Menzie <Alice.Menzie@gmail.com>
2021-07-13 10:50:09 +1000

    BOOKS CONTROLLER: created create method and book params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,14 @@ class BooksController < ApplicationController
         render json: Book.all
     end 
 
+    def create
+    @book = Book.new(book_params)
+    end 
 
+    private 
+
+def book_params
+ params.require(:book).permit(:author, :title, :year, :status, :genre)
+end 
 
 end
