fac08d25ad71b89a5975a0ce6957b7da3668a1e1
Alice Menzie <Alice.Menzie@gmail.com>
2021-07-14 14:40:36 +1000

    modified seed data added user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,54 +6,64 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Seed Starting"
+puts 'Seed Starting'
+
+user = User.create(username: 'alice',
+                   email: 'a@gmail.com',
+                   password: 'qwerty',
+                   password_confirmation: 'qwerty')
 
 book = Book.create(
-  author: "J.K. Rowling",
-  title: "Harry Potter and the Chamber of Secrets",
+  author: 'J.K. Rowling',
+  title: 'Harry Potter and the Chamber of Secrets',
   year: 1998,
-  genre: "Fantasy",
-  status: "unread",
+  genre: 'Fantasy',
+  status: 'unread',
+  user_id: 1
 )
 
 book = Book.create(
-  author: "Harper Lee",
-  title: "How to Kill a mockingbird",
+  author: 'Harper Lee',
+  title: 'How to Kill a mockingbird',
   year: 1960,
-  genre: "Fiction",
-  status: "unread",
+  genre: 'Fiction',
+  status: 'unread',
+  user_id: 1
 )
 
 book = Book.create(
-  author: "J. R. R. Tolkien",
-  title: "The Hobbit",
+  author: 'J. R. R. Tolkien',
+  title: 'The Hobbit',
   year: 1937,
-  genre: "Fiction",
-  status: "unread",
+  genre: 'Fiction',
+  status: 'unread'
 )
 
 book = Book.create(
-  author: "C. S. Lewis",
-  title: "The Lion, the Witch and the Wardrobe",
+  author: 'C. S. Lewis',
+  title: 'The Lion, the Witch and the Wardrobe',
   year: 1950,
-  genre: "Fiction",
-  status: "unread",
+  genre: 'Fiction',
+  status: 'unread',
+  user_id: 1
 )
 
 book = Book.create(
-  author: "J.K. Rowling",
-  title: "Harry Potter and the Philosophers Stone",
+  author: 'J.K. Rowling',
+  title: 'Harry Potter and the Philosophers Stone',
   year: 1997,
-  genre: "Fantasy",
-  status: "unread",
+  genre: 'Fantasy',
+  status: 'unread',
+  user_id: 1
 )
 
 book = Book.create(
-  author: "J.K. Rowling",
-  title: "Harry Potter and the Prisoner of Azkaban",
+  author: 'J.K. Rowling',
+  title: 'Harry Potter and the Prisoner of Azkaban',
   year: 1999,
-  genre: "Fantasy",
-  status: "unread",
+  genre: 'Fantasy',
+  status: 'unread',
+  user_id: 1
 )
 
-puts "Seed End"
+puts 'Seed End'
