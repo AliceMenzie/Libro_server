e571aa3e113d70637b05f94067e4f94b6fb416b3
Alice Menzie <Alice.Menzie@gmail.com>
2021-07-14 10:35:56 +1000

    added logic to user model signup and login methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
+  def sign_up
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   def login
   end
 
-  def sign_up
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :reviews
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'books#index'
+  root "books#index"
   resources :books, only: %i[index create update destroy]
 
-  post '/sign-up', to: 'users#sign-up'
-  post '/login', to: 'users#login'
+  post "/sign-up", to: "users#sign_up"
+  post "/login", to: "users#login"
 end
