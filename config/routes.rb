e7d077a57b01057fde6217bab742c78d78cae9b5
Alice Menzie <Alice.Menzie@gmail.com>
2021-07-14 10:19:06 +1000

    added signup and login methods to user controller, added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :reviews
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "books#index"
-  resources :books, only: [:index, :create, :update, :destroy]
+  root 'books#index'
+  resources :books, only: %i[index create update destroy]
+
+  post '/sign-up', to: 'users#sign-up'
+  post '/login', to: 'users#login'
 end
