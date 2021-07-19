4b35aff32e31ba2958d77c13c35f718301fbdb89
Mark Boyle <markboyle19@gmail.com>
2021-07-19 16:44:00 +1000

    moved /books/history route above books:resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :reviews
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "books#index"
+  get "/books/history", to: "books#history"
   resources :books
   # get '/wishlist', to: 'books#wishlist'
   get "/books/wishlist", to: "books#wishlist"
-  get "/books/history", to: "books#history"
   post "/sign-up", to: "users#sign_up"
   post "/login", to: "users#login"
 end
