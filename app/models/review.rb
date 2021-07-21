3723202263280676897658754127a4eb2f8fb56c
Alice Menzie <Alice.Menzie@gmail.com>
2021-07-21 16:24:05 +1000

    added testing (validations, associations, database) for review model, added validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
+  validates :description, length: { maximum: 300 }
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5  }
   belongs_to :book
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:book) { Book.new }
+  subject(:review) do
+    described_class.new(
+      description: 'this is a great book',
+      rating: 3,
+      book: book
+    )
+  end
+
+  it 'is valid with valid attributes' do
+    expect(review).to be_valid
+  end
+
+  describe 'Database tables' do
+    it { expect(review).to have_db_column(:description).of_type(:text) }
+    it { expect(review).to have_db_column(:rating).of_type(:integer) }
+    it { expect(review).to have_db_column(:book_id).of_type(:integer) }
+  end
+
+  describe 'validations' do
+    it { expect(review).to validate_length_of(:description) }
+    it { expect(review).to validate_numericality_of(:rating) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:book) }
+  end
 end
