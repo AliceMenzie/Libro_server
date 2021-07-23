b9db5e079a59cbde89df383278d92c2ee9c61e65
Mark Boyle <markboyle19@gmail.com>
2021-07-23 12:03:43 +1000

    Implemented image uploading with AWS S3

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
   validates :author, :title, :genre, :year, :status, presence: true
   has_one :review, dependent: :destroy
   belongs_to :user
+  has_one_attached :bookimage
 end
